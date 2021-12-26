6be235ba7715f8acf736cd0d33b9eed202bd82c3
Aaron Lin <aaronlin74@gmail.com>
2021-12-26 16:25:59 +1100

    Node server for learn animal web application

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const { connect } = require('mongoose')
+const app = express()
+const users = require('./routes/api/users')
+const reviews = require('./routes/api/reviews')
+const animals = require('./routes/api/animals')
+const auth = require('./routes/api/auth')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// Use Routes
+app.use('/api/users', users)
+app.use('/api/reviews', reviews)
+app.use('/api/animals', animals)
+app.use('/api/auth', auth)
+
+async function start() {
+  try {
+    await mongoose.connect(process.env.DB)
+    console.log('connected to database')
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('connect to server')
+    })
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+start()
