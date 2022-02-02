4f25bffe74d07f9a8a74be52df40f4d31bd30abb
Aaron Lin <aaronlin74@gmail.com>
2022-02-03 00:00:40 +1100

    Restructure the code to extract the getAnimals function

--- routes/api/animals.js
+++ routes/api/animals.js
@@ -2,31 +2,49 @@ const express = require('express')
 const router = express.Router()
 const Animal = require('../../models/animal')
 const authenticateToken = require('../../middleware/authenticateToken')
+const DEFAULT_LIMIT_NUMBER = 3
 
 // @route   GET api/animals?type=type&limit=3
 // @desc    Get limit number of animals of certain type using query string
 // @access  Public
-router.get('/', async (req, res) => {
-  // data transmited over the network is in string type
-  const limitNum = Number(req.query.limit) || 3
-  let animals
+// router.get('/', async (req, res) => {
+//   // data transmited over the network is in string type
+//   const limitNum = Number(req.query.limit) || 3
+//   let animals
+//   try {
+//     animals = await Animal.find({ type: req.query.type }).limit(limitNum)
+
+//     res.json(animals)
+//   } catch (err) {
+//     res.status(500).json({ message: err.message })
+//   }
+// })
+
+async function getAnimals(type, name, limitNum) {
   try {
-    animals = await Animal.find({ type: req.query.type }).limit(limitNum)
-
-    res.json(animals)
+    // If url has search query, find the animals which name contains the search term
+    if (name) {
+      animals = await Animal.find({
+        type: type,
+        name: { $regex: `${name}`, $options: 'i' },
+      }).limit(limitNum)
+    } else {
+      animals = await Animal.find({ type: type }).limit(limitNum)
+    }
+    return animals
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    console.log(err)
   }
-})
-
-// @route GET api/animals/mammal?limit=3
+}
+// @route GET api/animals/mammal?limit=3&search=name
 // @desc Get limit number of mammal animals
 // @access Public
 router.get('/mammal', async (req, res) => {
-  const limitNum = Number(req.query.limit) || 3
+  const limitNum = Number(req.query.limit) || DEFAULT_LIMIT_NUMBER
+  const name = req.query.search
   let animals
   try {
-    animals = await Animal.find({ type: 'mammal' }).limit(limitNum)
+    animals = await getAnimals('mammal', name, limitNum)
     res.json(animals)
   } catch (err) {
     res.status(500).json({ message: err.message })
@@ -37,10 +55,11 @@ router.get('/mammal', async (req, res) => {
 // @desc Get limit number of bird animals
 // @access Public
 router.get('/bird', async (req, res) => {
-  const limitNum = Number(req.query.limit) || 3
+  const limitNum = Number(req.query.limit) || DEFAULT_LIMIT_NUMBER
+  const name = req.query.search
   let animals
   try {
-    animals = await Animal.find({ type: 'bird' }).limit(limitNum)
+    animals = await getAnimals('bird', name, limitNum)
     res.json(animals)
   } catch (err) {
     res.status(500).json({ message: err.message })
@@ -51,10 +70,11 @@ router.get('/bird', async (req, res) => {
 // @desc Get limit number of reptile animals
 // @access Private
 router.get('/reptile', authenticateToken, async (req, res) => {
-  const limitNum = Number(req.query.limit) || 3
+  const limitNum = Number(req.query.limit) || DEFAULT_LIMIT_NUMBER
+  const name = req.query.search
   let animals
   try {
-    animals = await Animal.find({ type: 'reptile' }).limit(limitNum)
+    animals = await getAnimals('reptile', name, limitNum)
     res.json(animals)
   } catch (err) {
     res.status(500).json({ message: err.message })
@@ -65,10 +85,11 @@ router.get('/reptile', authenticateToken, async (req, res) => {
 // @desc Get limit number of fish animals
 // @access Private
 router.get('/fish', authenticateToken, async (req, res) => {
-  const limitNum = Number(req.query.limit) || 3
+  const limitNum = Number(req.query.limit) || DEFAULT_LIMIT_NUMBER
+  const name = req.query.search
   let animals
   try {
-    animals = await Animal.find({ type: 'fish' }).limit(limitNum)
+    animals = await getAnimals('fish', name, limitNum)
     res.json(animals)
   } catch (err) {
     res.status(500).json({ message: err.message })
@@ -79,10 +100,11 @@ router.get('/fish', authenticateToken, async (req, res) => {
 // @desc Get limit number of amphibian animals
 // @access Private
 router.get('/amphibian', authenticateToken, async (req, res) => {
-  const limitNum = Number(req.query.limit) || 3
+  const limitNum = Number(req.query.limit) || DEFAULT_LIMIT_NUMBER
+  const name = req.query.search
   let animals
   try {
-    animals = await Animal.find({ type: 'amphibian' }).limit(limitNum)
+    animals = await getAnimals('amphibian', name, limitNum)
     res.json(animals)
   } catch (err) {
     res.status(500).json({ message: err.message })
@@ -93,10 +115,11 @@ router.get('/amphibian', authenticateToken, async (req, res) => {
 // @desc Get limit number of invertebrate animals
 // @access Private
 router.get('/invertebrate', authenticateToken, async (req, res) => {
-  const limitNum = Number(req.query.limit) || 3
+  const limitNum = Number(req.query.limit) || DEFAULT_LIMIT_NUMBER
+  const name = req.query.search
   let animals
   try {
-    animals = await Animal.find({ type: 'invertebrate' }).limit(limitNum)
+    animals = await getAnimals('invertebrate', name, limitNum)
     res.json(animals)
   } catch (err) {
     res.status(500).json({ message: err.message })
